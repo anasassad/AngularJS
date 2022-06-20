4e81a8ad68c13a59250169d0eb5745bf2af2bd4e
Anasassad <Lwalida1921@gmail.com>
2022-06-20 16:20:33 +0100

    2 way and 1 way binding

--- js/app.js
+++ js/app.js
@@ -4,21 +4,29 @@
 
     var app = angular.module("EntryMainModule", []);
 
-    app.controller("DIController",DIController);
+    app.controller("BindingController",BindingController);
 
     //Inject the instance of scope and filter inside our function called DIController
     // ... to avoid the non-function of our js when we will minify it later
-    DIController.$inject = ['$scope','$filter'];
+    BindingController.$inject = ['$scope'];
 
-    function DIController($scope,$filter) {
+    function BindingController($scope) {
 
-        $scope.name = "Ahmed";
-        $scope.upperCase = function(){
-            var upper = $filter('uppercase');
-            $scope.name = upper($scope.name);
+        $scope.showNumberOfWatchers = function(){
+            console.log("Watchers ",$scope.$$watchersCount);
+        }
+
+        $scope.LogFirstName = function(){
+            console.log("First Name : ",$scope.firstname);
+        }
+
+        $scope.LogFullName = function(){
+            console.log("Full Name : ",$scope.fullname);
+        }
+
+        $scope.setFullName = function(){
+            $scope.fullname = $scope.firstname + " " + "ASSAD";
         }
     }
-})()
 
-// Minified Code
-//(function(){var app=angular.module("EntryMainModule",[]);app.controller("DIController",DIController);DIController.$inject=['$scope','$filter'];function DIController($scope,$filter){$scope.name="Ahmed";$scope.upperCase=function(){var upper=$filter('uppercase');$scope.name=upper($scope.name)}}})()
+})()
