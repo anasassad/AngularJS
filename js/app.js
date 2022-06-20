c4b20ae58d11b122b5485977d5849570eeebdce7
Anasassad <Lwalida1921@gmail.com>
2022-06-20 15:09:52 +0100

    Defining 3 ways to solve the out of context while using setTimeout

--- js/app.js
+++ js/app.js
@@ -4,27 +4,45 @@
 
     var app = angular.module("EntryMainModule", []);
 
-    app.controller("MainController",MainController);
+    app.controller("CounterController",CounterController);
 
     //Inject the instance of scope and filter inside our function called DIController
     // ... to avoid the non-function of our js when we will minify it later
-    MainController.$inject = ['$scope'];
+    CounterController.$inject = ['$scope','$timeout'];
 
-    function MainController($scope) {
-            $scope.counter = 0 ;
+    function CounterController($scope,$timeout) {
+
+            $scope.counter = 0 ; 
 
             $scope.increaseByOne = function () {
-                $scope.counter++;
-            }
+                    //Solution 3 is about declaring a service called $timeout that is comes with Angular JS
+                    $timeout(function () {
+    
+                        $scope.counter++;
+    
+                    },500);
+                };
 
-            $scope.$watch(function(){
-                console.log("Digest Notification for watchers");
-            });
+            // $scope.increaseByOne = function () {
+            //         setTimeout(function () {
+                                    
+            //          //The function $apply have its own $digest in the hoods so this approach is better than the one below
+            //          $scope.$apply(function(){
+            //              $scope.counter++;
+            //          });
+            //         },500);
+            // };
 
-            // $scope.$watch('counter',function(newValue , oldValue){
-            //     console.log("oldValue",oldValue);
-            //     console.log("newValue",newValue);
-            // })
+            // //First solution to make sure the prop counter is incremented
+            // $scope.increaseByOne = function () {
+            //     setTimeout(function () {
+
+            //         $scope.counter++;
+            //         $scope.$digest();
+
+            //     },500);
+            // }
+
     }
 
 })()
